e9285b2a8917cd217a9f6dafd6021247f6055c7d
Shaun Kendrone <kendroneshaun@gmail.com>
2023-07-04 15:35:57 +0300

    Adding Hello World to Ticketing

--- src/org/pahappa/systems/ticketing/Ticketing.java
+++ src/org/pahappa/systems/ticketing/Ticketing.java
@@ -1,11 +1,12 @@
 package org.pahappa.systems.ticketing;
 
-import org.pahappa.systems.ticketing.views.TicketView;
+// import org.pahappa.systems.ticketing.views.TicketView;
 
 public class Ticketing {
 
     public static void main(String[] args) {
-        TicketView ticketView = new TicketView();
-        ticketView.displayMenu();
+        // TicketView ticketView = new TicketView();
+        // ticketView.displayMenu();
+        System.out.println("Hello World");
     }
 }
